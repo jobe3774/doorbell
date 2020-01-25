43e29665d81ea15251498bba402924bba07874ac
jobe <jobe@jobe-software.de>
2020-01-25 12:51:08 +0100

    Supply GPIO mode via command line.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # doorbell
-A [raspend](https://github.com/jobe3774/raspend) based application to switch on/off my doorbell. Therefore it exposes two methods of the **GPIOSwitch** class, which is included in this project, two switch on/off a GPIO pin. This GPIO pin controls a relay, which in turn controls the power supply of my bell transformer. Using the command interface of [raspend](https://github.com/jobe3774/raspend) I can call these methods via HTTP POST or GET requests.
+A [raspend](https://github.com/jobe3774/raspend) based application to switch on/off my doorbell. 
+Therefore it exposes two methods of the **GPIOSwitch** class, which is included in this project, to switch on/off a GPIO pin. 
+This GPIO pin controls a relay, which in turn controls the power supply of my bell transformer. 
+Using the command interface of [raspend](https://github.com/jobe3774/raspend) I can call these methods via HTTP POST or GET requests.
 
 # License
 MIT. See LICENSE file.
--- main.py
+++ main.py
@@ -26,7 +26,8 @@ def main():
     # Check commandline arguments.
     cmdLineParser = argparse.ArgumentParser(prog="doorbell", usage="%(prog)s [options]")
     cmdLineParser.add_argument("--port", help="The port the server should listen on", type=int, required=True)
-    cmdLineParser.add_argument("--gpio", help="The GPIO pin to use", type=int, required=True)
+    cmdLineParser.add_argument("--gpioPin", help="The GPIO pin to use", type=int, required=True)
+    cmdLineParser.add_argument("--gpioMode", help="The GPIO mode to use (0: BOARD, 1: BCM)", type=int, required=True, default=0)
 
     try: 
         args = cmdLineParser.parse_args()
@@ -35,7 +36,12 @@ def main():
 
     try:
         # A relay is controlled via GPIO pin, which in turn controls the power supply of my bell transformer.
-        doorBell = GPIOSwitch(GPIO.BOARD, args.gpio, GPIO.HIGH, False, False)
+        mode = GPIO.BOARD
+
+        if args.gpioMode == 1:
+            mode = GPIO.BCM
+
+        doorBell = GPIOSwitch(mode, args.gpioPin, GPIO.HIGH, False, False)
 
         myApp = RaspendApplication(args.port)
 
@@ -53,7 +59,7 @@ def main():
         logging.exception("Unexpected error occured!", exc_info = True)
 
     finally:
-        # Release pin #12.
+        # Release GPIO.
         doorBell.cleanup()
 
 if __name__ == "__main__":
